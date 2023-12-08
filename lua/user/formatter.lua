4cff16139c98fad1b752cc2fc46396d634d764db
kunring <25564242+kunring@users.noreply.github.com>
2023-12-08 07:19:15 +0200

    auto select between deno and prettier

--- lua/user/formatter.lua
+++ lua/user/formatter.lua
@@ -1,15 +1,29 @@
+local function is_deno()
+	return require("lspconfig").util.root_pattern("deno.json", "deno.jsonc") ~= nil
+end
+
+local function denoprettier(filetype)
+	return function()
+		if is_deno() then
+			return require("formatter.filetypes." .. filetype).denofmt()
+		else
+			return require("formatter.filetypes." .. filetype).prettier()
+		end
+	end
+end
+
 require("formatter").setup({
 	logging = true,
 	log_level = vim.log.levels.WARN,
 	filetype = {
 		typescript = {
-			require("formatter.filetypes.typescript").prettier,
+			denoprettier("typescript"),
 		},
 		javascript = {
-			require("formatter.filetypes.javascript").prettier,
+				denoprettier("javascript")
 		},
 		json = {
-			require("formatter.filetypes.json").prettier,
+				denoprettier("json")
 		},
 		html = {
 			require("formatter.filetypes.html").prettier,
