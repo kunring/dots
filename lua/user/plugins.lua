5c685869aabfff3db6ec204dc98feab1476b933c
kunring <kunring@proton.me>
2023-12-18 22:18:00 +0200

    formatting

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -4,14 +4,14 @@
 -- Bootstrap plugin manager if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-	"--filter=blob:none",
-	"https://github.com/folke/lazy.nvim.git",
-	"--branch=stable", -- latest stable release
-	lazypath,
-})
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,16 +22,16 @@ require("lazy").setup({
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		config = function () 
+		config = function()
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
 				ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "typescript", "rust", "python" },
 				sync_install = false,
 				highlight = { enable = true },
-				indent = { enable = false },  
+				indent = { enable = false },
 			})
-		end
+		end,
 	},
 	{
 		"iamcco/markdown-preview.nvim",
@@ -61,5 +61,3 @@ require("lazy").setup({
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 })
-
-
