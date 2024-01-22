0c84fbed8ff84183dc3ef2d9849494dbc78ae497
kunring <kunring@proton.me>
2024-01-22 22:03:06 +0200

    remove redundant comments

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,7 +1,3 @@
--- Use lazy.nvim as the plugin manager
--- https://github.com/folke/lazy.nvim
-
--- Bootstrap plugin manager if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -9,16 +5,15 @@ if not vim.loop.fs_stat(lazypath) then
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		"--branch=stable",
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Start plugin manager
 require("lazy").setup({
 	"folke/lazy.nvim",
-	"Raimondi/delimitMate", -- Bracket matching
+	"Raimondi/delimitMate",
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
