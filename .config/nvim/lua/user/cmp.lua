a7ba340744404c088f084067d5478610d11364db
kunring <kunring@proton.me>
2024-06-12 19:24:06 +0300

    nvim: disable cmp for comments

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -111,4 +111,12 @@ cmp.setup({
 		ghost_text = false,
 		native_menu = false,
 	},
+	enabled = function()
+		local context = require("cmp.config.context")
+		if vim.api.nvim_get_mode().mode == "c" then
+			return true
+		else
+			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+		end
+	end,
 })
