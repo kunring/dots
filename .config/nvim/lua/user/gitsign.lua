62391a93836c8732e9c164b2175fabca0b20ddd5
kunring <kunring@proton.me>
2024-09-10 14:47:15 +0300

    nvim: update gitsigns config according to interface changes

--- .config/nvim/lua/user/gitsign.lua
+++ .config/nvim/lua/user/gitsign.lua
@@ -29,9 +29,7 @@ gitsigns.setup({
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter_opts = {
-		relative_time = false,
-	},
+	current_line_blame_formatter = "<author>, <author_time:%c> - <summary>",
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil,
@@ -42,8 +40,5 @@ gitsigns.setup({
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 })
